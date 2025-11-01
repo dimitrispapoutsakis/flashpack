f8c6552e2d88022775e5318dfea9603f9a5a4ecd
Dimitris <dimitrispapoutakis@gmail.com>
2025-11-01 04:29:06 +0200

    feat: Perists ui store.

--- src/renderer/src/store/uiStore.ts
+++ src/renderer/src/store/uiStore.ts
@@ -1,11 +1,21 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface IUiState {
 	selectedPlayer: string;
 	setSelectedPlayer: (player: string | null) => void;
 }
 
-export const useUiStore = create<IUiState>((set) => ({
-	selectedPlayer: "Select Device",
-	setSelectedPlayer: (player) => set({ selectedPlayer: player }),
-}));
+export const useUiStore = create<IUiState>()(
+	persist(
+		(set) => ({
+			selectedPlayer: "Select Device",
+			setSelectedPlayer: (player) =>
+				set({ selectedPlayer: player ?? "Select Device" }),
+		}),
+		{
+			name: "ui-storage",
+			storage: createJSONStorage(() => localStorage),
+		},
+	),
+);
